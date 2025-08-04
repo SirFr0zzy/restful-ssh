a3452eb84352134e9e70539cc90a94d8faddaaa5
SirFr0zzy <me@elias-lauterbach.de>
2025-08-04 04:10:11 +0200

    Add session creation route with SSH connection handling

---
+++ routes/session/create_session_route.py
@@ -0,0 +1,66 @@
+# File was created on 04.08.25 at 03:15 by Elias Lauterbach
+
+from flask import jsonify, Blueprint, request
+from sqlalchemy.orm import sessionmaker
+
+import app
+from app import auth, require_args
+import os
+from session import Session
+import paramiko
+
+handler = Blueprint('/session', __name__)
+
+
+@handler.route('/session', methods=['POST'])
+@require_args('host', 'port', 'username')
+def action():
+    host = request.args.get('host')
+    port = int(request.args.get('port'))
+    user = request.args.get('username')
+    password = request.args.get('password')
+    ssh_key = request.args.get('ssh_key')
+    client_ip = app.get_client_ip()
+
+    agent_id = app.session_id
+
+    print(f"Agent Session: {agent_id}")
+
+
+    engine = app.get_database_engine()
+    SessionLocal = sessionmaker(bind=engine)
+    session = SessionLocal()
+
+    _session = Session(
+        host = host,
+        port = port,
+        user = user,
+        password = password,
+        private_key = ssh_key,
+        owner_ip=client_ip,
+        agent_session_uuid=agent_id
+    )
+
+    session.add(_session)
+    session.commit()
+
+    session_id = _session.id
+
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # unsichere Hosts automatisch akzeptieren
+
+    if ssh_key:
+        client.connect(hostname=host, port=port, username=user, pkey=ssh_key)
+    else:
+        client.connect(hostname=host, port=port, username=user, password=password)
+
+    app.sessions[session_id] = client
+
+    return jsonify({
+        "session_id": session_id
+    }), 200
+
+
+
+
+
